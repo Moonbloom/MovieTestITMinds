afcdae5b1ed076a34c8b9655b268d9753739cc0e
Moonboom <kasperliholm@hotmail.com>
2014-08-25 15:56:16 +0200

    First commit

---
+++ MvcMovie/Scripts/KOEdit/KOEditPopulate.js
@@ -0,0 +1,32 @@
+﻿var urlPath = "/Movies";
+ 
+$(function () {
+    ko.applyBindings(KOEdit);
+    KOEdit.loadArticles();
+});
+
+var KOEdit = {
+    Movies: ko.observableArray([]),
+ 
+    loadArticles: function () {
+        var self = this;
+        //Ajax Call Get All Movies
+        $.ajax({
+            type: "GET",
+            url: urlPath + '/FillIndex',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                $(data).each(function () {
+                    var isolatedDateString = this.ReleaseDate.substr(6, (this.ReleaseDate.length - (2 + 6)))
+                    this.ReleaseDate = new Date(parseInt(isolatedDateString)).toLocaleDateString();
+                });
+
+                self.Movies(data); //Put the response in ObservableArray
+            },
+            error: function (err) {
+                alert(err.status + " : " + err.statusText);
+            }
+        });
+    }
+};
